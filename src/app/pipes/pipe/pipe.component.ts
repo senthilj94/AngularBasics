05b10dfa732bd96858c1c8a6144457ea5a554c61
senthilj94 <senthilj94@users.noreply.github.com>
2020-04-18 18:16:12 +0530

    async pipe

--- src/app/pipes/pipe/pipe.component.ts
+++ src/app/pipes/pipe/pipe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, interval, Subscriber } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pipe',
@@ -14,7 +17,25 @@ export class PipeComponent implements OnInit {
   users = [{name: 'senthil', gender: 'm'},
            {name: 'bala', gender: 'm'},
            {name: 'neelu', gender: 'f'}];
-  constructor() { }
+
+  // jsonData = this.http.get('https://jsonplaceholder.typicode.com/todos/1');
+
+  // jsonData = new Promise((resolve, reject) => {
+  //   this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+  //   .subscribe(x => resolve(x));
+  // });
+
+  jsonData = new Observable(subscriber => {
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+    .subscribe(x => subscriber.next(x));
+  });
+
+  date$ = interval(1000).pipe(
+    map(x => new Date()),
+    take(10)
+  );
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
